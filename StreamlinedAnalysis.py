23136eb741ce749c7332e1a30f8ced5f38370d3a
killianmcginley <148297445+killianmcginley@users.noreply.github.com>
2023-10-18 04:39:43 +0000

    Committing the analysis script changes

--- StreamlinedAnalysis.py
+++ StreamlinedAnalysis.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-data = pd.read_csv('Construction_Data_PM_Tasks_All_Projects.csv')
-print(data.head())
-overdue_tasks = data[data['OverDue'] == 'Yes']  # Assuming 'OverDue' column contains string 'Yes' for overdue tasks
+
+df = pd.read_csv('Construction_Data_PM_Tasks_All_Projects.csv')
+print(df.head())
+
+overdue_tasks = df[df['OverDue'] == 'Yes']  # Ensured correct spelling of 'OverDue' column
 total_overdue = len(overdue_tasks)
 print(f"Total number of tasks that are overdue: {total_overdue}")
-task_counts = data[data['Status'].isin(['Open', 'Closed'])].groupby(['Task Group', 'Status']).size().unstack()
+
+task_counts = df[df['Status'].isin(['Open', 'Closed'])].groupby(['Task Group', 'Status']).size().unstack()
 print(task_counts)
+
 task_counts.plot(kind='bar', stacked=False, figsize=(12, 8))
 plt.title('Total Number of Open and Closed Tasks by Each Task Group')
 plt.ylabel('Number of Tasks')
 plt.xlabel('Task Group')
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.savefig('/workspaces/GENAI/task_group_chart.png')
+plt.savefig('task_group_chart.png')
 plt.close()
+
 overdue_by_project = overdue_tasks.groupby('project').size()
 plt.figure(figsize=(12, 6))
 overdue_by_project.plot(kind='bar', color='skyblue', edgecolor='black')
@@ -25,7 +29,3 @@ plt.ylabel('Number of Overdue Tasks')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
-os.system("cd /workspaces/GENAI")
-os.system("git add .")
-os.system('git commit -m "Committing the analysis script changes"')
-os.system("git push")
